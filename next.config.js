63c634ff4328be13b702ed250b66a704aeb52bbe
Justin Tran <justintran952@gmail.com>
2024-01-11 20:55:01 -0500

    Add SVG loader to webpack config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ module.exports = {
       },
     ],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test : /\.svg$/,
+      use: [{loader: '@svgr/webpack', options: {icon: true } }],
+    });
+    return config;
+  },
 }
 
 
