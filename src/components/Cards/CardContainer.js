6e0862e1e8297fd07c769a1ea59bc92027cabc93
Knightn4 <Justintran952@gmail.com>
2024-01-13 05:49:09 -0500

    Added proper navigation

--- src/components/Cards/CardContainer.js
+++ src/components/Cards/CardContainer.js
@@ -1,16 +1,22 @@
-import { Meteors } from '../Ui/Meteors';
+
+
 import Card from './Card';
 import {getResources} from '@/utils/contentful';
 
+export default async function CardContainer({ category }) {
 
-export default async function CardContainer() {
   const resources = await getResources();
+
+
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 mt-10">
       {resources.map((resource) => {
-        return (
-        <Card key={resource.sys.id} resource={resource}/>
-      )})}
+        if (category === resource.tag) {
+          return <Card key={resource.slug} resource={resource} />;
+        } else if (category === undefined) {
+          return <Card key={resource.slug} resource={resource} />;
+        }
+      })}
     </div>
   );
 }
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,36 +1,60 @@
-
 "use client";
-import Link from 'next/link';
-const ButtonsComponent = () => {
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 
 
-  const buttons = [
-    { label: "All", value: "/" },
-    { label: "Languages", value: "languages" },
-    { label: "Technologies", value: "technologies" },
-    { label: "YT Creators", value: "ytCreators" },
-    { label: "Learning Tools", value: "learningTools" },
-    { label: "Inspirations", value: "inspirations" },
-    { label: "UI Components", value: "uiComponents" },
-  ];
+const Tabs = ({ category }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("category");
+  const [active, setActive] = useState("All");
 
-  
+
+  useEffect(() => {
+    // Set active to "All" when URL is / or has no category
+    if (router.asPath === "/" || !search) {
+      setActive("All");
+    } else {
+      setActive(search);
+    }
+  }, [router.asPath, search]);
 
   return (
-    <div className ="flex flex-wrap gap-5 justify-center">
-      {buttons.map((button) => (
+    <div className="flex flex-wrap gap-5 justify-center">
+      {category.map((categories) => (
         <button
-          key={button.value}
-          className={` bg-black hover:border-white hover:border-opacity-[40%]  transition-colors ease-in duration-300 text-white  text-base border-[1px] border-navy border-opacity-[100%] font-medium py-2 px-4 rounded-[14px]`}
-          onClick={() => setActive(button.label)}
+          key={categories}
+          className={`relative hidden md:block text-base transition ${
+            active === categories && search === categories
+              ? ""
+              : "hover:text-white/60"
+          } focus-visible:outline-2 font-medium py-2 px-4 rounded-[16px]`}
+          style={{
+            WebkitTapHighlightColor: "transparent",
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            setActive(categories);
+            categories === "All"
+              ? router.push("/",{scroll:false})
+              : router.push(`/?category=${categories}`, { scroll: false });
+          }}
         >
-        {button.label}
+          {categories === active && (
+            <motion.span
+              layoutId="bubble"
+              className="absolute inset-0 z-10 bg-gradient-to-r from-gold to-white mix-blend-difference"
+              style={{ borderRadius: 9999 }}
+              transition={{ type: "spring", bounce: 0.3, duration: 0.7 }}
+            />
+          )}
+          {categories}
         </button>
       ))}
     </div>
   );
 };
 
-export default ButtonsComponent;
-
+export default Tabs;
