9a23d3165b9ba5a9884e5ff1c6db10a93d53e309
Justin Tran <justintran952@gmail.com>
2024-02-01 00:57:52 -0500

    Changed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter, PT_Serif } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import { Footer } from "@/components/Footer/Footer";
@@ -15,7 +15,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className ={inter.className}>
        <head>
-        <link rel="icon" type="image" href="images/favicon-32x32.png"/>
+        <link rel="icon" type="image/svg+xml" href="/images/favicon.svg"></link>
+        <link rel="icon" type="image/png" href="/images/favicon.png"></link>
       </head>
       <body className="px-[6%] md:px-[8%] font-inter h-[50rem] w-full dark:bg-black bg-black dark:bg-dot-white/[0.08] bg-dot-white/[0.08] relative box-border">
         <Navbar />
